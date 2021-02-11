80c5d0cfc1d01a66c283d7f929d8e41b701a66e7
AlicizationHash <13465717387@163.com>
2021-02-10 22:58:01 -0500

    scripts: inital commit

---
+++ scripts/jenkins.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+# Add this file to /etc/rc.d/rc.local
+
+# Add jenkins jar directory 
+export JENKINS_DIR=$HOME/jenkins
+ 
+# Switch to Jenkins directory
+cd $JENKINS_DIR
+
+# Check pid 
+pid=`ps -ef | grep jenkins.war | grep -v 'grep'| awk '{print $2}'`
+   if [ "$1" = "start" ];then
+   if [ -n "$pid" ];then
+   echo 'jenkins is running...'
+ else
+# Run Web
+   nohup java -jar $JENKINS_HOME/jenkins.war --httpPort=80 >/dev/null &
+   fi
+   elif [ "$1" = "stop" ];then
+   exec ps -ef | grep jenkins | grep -v grep | awk '{print $2}'| xargs kill -9
+   echo 'jenkins is stop...'
+ else
+   echo "Please input like this:"./jenkins.sh start" or "./jenkins stop""
+   fi 
