d0b8b7cb5715fb4283a0c2f4f62210393a0aea62
EndCredits <alicization.han@gmail.com>
2021-02-11 01:11:44 -0500

    scripts: Changed to correct path

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -4,7 +4,7 @@
 # Add this file to /etc/rc.d/rc.local
 
 # Add jenkins jar directory 
-export JENKINS_DIR=$HOME/jenkins
+export JENKINS_DIR=/var/jk-scrpits/JenkinsAutobuild
 # Set http port
 HTTP_PORT=8080 
 
@@ -18,7 +18,7 @@ pid=`ps -ef | grep jenkins.war | grep -v 'grep'| awk '{print $2}'`
    echo 'jenkins is running...'
  else
 # Run Web
-   nohup java -jar $JENKINS_HOME/jenkins.war --httpPort=$HTTP_PORT >/dev/null &
+   nohup java -jar $JENKINS_DIR/jenkins.war --httpPort=$HTTP_PORT >/dev/null &
    fi
    elif [ "$1" = "stop" ];then
    exec ps -ef | grep jenkins | grep -v grep | awk '{print $2}'| xargs kill -9
