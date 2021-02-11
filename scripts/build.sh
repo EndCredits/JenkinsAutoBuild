e6b5bd8195c3e99620826d5d55a2612d9cd8c956
EndCredits <alicization.han@gmail.com>
2021-02-11 02:38:16 -0500

    scripts: Add build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export IANDROID_SRC_PATH=/root/android
+export ISYNC_FLAGS='-j$(nproc --all) -c --force-sync --no-clone-bundle --no-tags'
+export ICUSTOM_ROM_PREFIX=aosp
+export IBUILD_DEVICE=tiffany
+export IBUILD_TYPE=userdebug
+export ILUNCH=$ICUSTOM_ROM_PREFIX"_"$IBUILD_DEVICE"-"$IBUILD_TYPE
+export IBUILD_THREAD=$(nproc --all)
+
+cd $IANDROID_SRC_PATH
+source build/envsetup.sh
+repo sync $ISYNC_FLAGS
+lunch $ILUNCH
+m bacon -j$IBUILD_THREAD
